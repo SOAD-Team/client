2d409ec2ff57884120e687fab00ea518cc9a5dae
Francisco <fmurillom@gmail.com>
2020-11-03 22:39:58 -0600

    [Fix] Code Smells

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -76,7 +76,7 @@ export default class Register extends Component {
     else{
       const stateValue : stateValue = {
         ...this.state,
-        passMatch : true
+        passMatch : false
       }
     }
   }
