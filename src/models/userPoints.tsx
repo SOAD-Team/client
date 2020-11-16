999f26de617f3af417b724255d92df599c59abef
Gabriel Abarca <gababarca@hotmail.com>
2020-11-16 04:30:33 -0600

    [Add] Recommendation Service and Helper

---
+++ src/models/userPoints.tsx
@@ -0,0 +1,7 @@
+export interface UserPoints{
+    imdb: number;
+    metaScore: number;
+    community: number;
+    platFav: number;
+    popularity: number;
+}
--- src/services/recomendationService.tsx
+++ src/services/recomendationService.tsx
@@ -1,6 +1,19 @@
 import * as Constants from '../constants'
-import axios, {AxiosResponse} from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { IRecommendation } from '../models/recommendation';
+import { UserPoints } from '../models/userPoints';
 
 export class RegisterService {
-  private static URL = Constants.apiUrl;
+    private static URL = `${Constants.apiUrl}recommendation`;
+
+    public static getMovieRecommendation(imdb: number, metaScore: number, community: number, platFav: number, popularity: number): Promise<AxiosResponse<IRecommendation>> {
+        const points: UserPoints = {
+            imdb,
+            metaScore,
+            community,
+            platFav,
+            popularity
+        }
+        return axios.post(this.URL, points);
+    }
 }
